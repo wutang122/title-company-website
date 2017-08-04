ccd50790bef7ebc133c40b694c0903d4cdb22716
Kevin Wojton <your.kfwojton@gmail.com>
2017-08-03 23:55:39 -0400

    new push;

--- kevinwojton/techparcel/wsgi.py
+++ kevinwojton/techparcel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hub3 project.
+WSGI config for Cleveland Rocks project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hub3.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techparcel.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
